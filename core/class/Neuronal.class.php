dfd13bbc190f59efd407379fe216d32f272a6bda
mika-nt28 <michaelhalbout@gmail.com>
2016-06-09 13:50:11 +0200

    Correction bug

--- core/class/Neuronal.class.php
+++ core/class/Neuronal.class.php
@@ -36,7 +36,7 @@ class Neuronal extends eqLogic {
 		$cmd .= ' >> ' . log::getPathToLog('Neuronal_update') . ' 2>&1 &';
 		exec($cmd);
 	}  
-	public function cron(){
+	public static function cron(){
 		if($this->getConfiguration('calibration')){
 			$listener = listener::byClassAndFunction('Neuronal', 'CreateApprentissageTable');
 			if (!is_object($listener)) {
