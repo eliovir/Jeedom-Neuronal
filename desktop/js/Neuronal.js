4c3d93fd11ccdd88e259e5edb49b32ca59fcacd7
mika-nt28 <michaelhalbout@gmail.com>
2016-06-09 10:40:00 +0200

    Correction Bug

--- desktop/js/Neuronal.js
+++ desktop/js/Neuronal.js
@@ -18,34 +18,32 @@ $('body').on('click','.bt_del',function(){
 $("#table_cmd_Entree").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 $("#table_cmd_Sortie").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 function addCmdToTable(_cmd) {
-
 	if (!isset(_cmd)) {
-        var _cmd = {};
-    }
-    if (!isset(_cmd.configuration)) {
-        _cmd.configuration = {};
-    }
-    var Table=$("#table_cmd_Entree");
-    if(_cmd.name!="Entree")
-    	Table=$("#table_cmd_Sortie");
-    	$.each( _cmd.configuration,function(){
-    		addCmdToTable(Table);
-    	})
-    	Table.setValues(_cmd, '.cmdAttr');
+ 		var _cmd = {};
+	}
+	if (!isset(_cmd.configuration)) {
+		_cmd.configuration = {};
+	}
+	var Table=$("#table_cmd_Entree");
+	if(_cmd.name!="Entree")
+		Table=$("#table_cmd_Sortie");
+    $.each( _cmd.configuration,function(){
+		addToTable(Table);
+	})
+  Table.setValues(_cmd, '.cmdAttr');
 }
-function addCmdToTable(Table) {
-		var tr =$('<tr>');
-  		tr.append($('<td>')
-	  		.append($('<input class="cmdAttr form-control input-sm " data-l1key="configuration" data-l2key="id" data-l3key="name" style="width:85%;display: inline-block;margin: 5px;">'))
-	  		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-default btn-xs cursor bt_selectCmdExpression" title="Rechercher une commande">')
-				.append($('<i class="fa fa-list-alt">'))));
-		  //	tr.append($('<td>')
-		  //		.append($('<input class="cmdAttr form-control input-sm " data-l1key="configuration" data-l1key="configuration" data-l2key="id" data-l3key="tolerance">')));
-		  	tr.append($('<td>')
-		  		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-success btn-xs cursor bt_add" title="Ajouter une commande">')
-					.append($('<i class="fa fa-plus-circle">')))
-		  		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-danger btn-xs cursor bt_del" title="Supprimer une commande">')
-					.append($('<i class="fa fa-minus-circle">'))));
-		
-			Table.append(tr);
+function addToTable(_Table) {
+	var tr =$('<tr>');
+  	tr.append($('<td>')
+		.append($('<input class="cmdAttr form-control input-sm " data-l1key="configuration" data-l2key="id" data-l3key="name" style="width:85%;display: inline-block;margin: 5px;">'))
+		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-default btn-xs cursor bt_selectCmdExpression" title="Rechercher une commande">')
+			.append($('<i class="fa fa-list-alt">'))));
+//	tr.append($('<td>')
+//		.append($('<input class="cmdAttr form-control input-sm " data-l1key="configuration" data-l1key="configuration" data-l2key="id" data-l3key="tolerance">')));
+	tr.append($('<td>')
+		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-success btn-xs cursor bt_add" title="Ajouter une commande">')
+			.append($('<i class="fa fa-plus-circle">')))
+		.append($('<a style="display : inline-block;margin:5px;" class="btn btn-danger btn-xs cursor bt_del" title="Supprimer une commande">')
+			.append($('<i class="fa fa-minus-circle">'))));
+	_Table.append(tr);
 }
