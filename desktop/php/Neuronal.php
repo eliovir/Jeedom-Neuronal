2803745bcc1feb0e97588f18df9e66bf7a6a005d
mika-nt28 <michaelhalbout@gmail.com>
2016-06-10 08:21:09 +0200

    Ajout d'un table de calibration

--- desktop/php/Neuronal.php
+++ desktop/php/Neuronal.php
@@ -48,92 +48,125 @@ $eqLogics = eqLogic::byType('Neuronal');
 		</div>
 	</div>
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-		<form class="form-horizontal">
-			<fieldset>
-				<legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-				<div class="form-horizontal">
-					<div class="form-group">
-						<label class="col-md-2 control-label">{{Nom du groupe}}</label>
-						<div class="col-sm-3">
-							<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-							<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du groupe}}"/>
-					
+		<div class="right">
+			<div class="col-sm-6">
+				<form class="form-horizontal">
+					<fieldset>
+						<legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+						<div class="form-horizontal">
+							<div class="form-group">
+								<label class="col-md-2 control-label">{{Nom du groupe}}</label>
+								<div class="col-sm-3">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du groupe}}"/>
+							
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-2 control-label" >{{Objet parent}}</label>
+								<div class="col-sm-3">
+									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+										<option value="">{{Aucun}}</option>
+										<?php
+											foreach (object::all() as $object) {
+												echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+											}
+										?>
+									</select>
+								</div>
+							</div>	
+							<div class="form-group">
+								<label class="col-sm-2 control-label" ></label>
+								<div class="col-sm-9">
+									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-2 control-label">{{Catégorie}}</label>
+								<div class="col-md-8">
+									<?php
+										foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+											echo '<label class="checkbox-inline">';
+											echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+											echo '</label>';
+										}
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-2 control-label" >{{Calibration}}</label>
+								<div class="col-sm-9">
+									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Calibration}}" data-l1key="configuration" data-l2key="calibration" />
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-2 control-label" >{{Objet parent}}</label>
-						<div class="col-sm-3">
-							<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-								<option value="">{{Aucun}}</option>
-								<?php
-									foreach (object::all() as $object) {
-										echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-									}
-								?>
-							</select>
+					</fieldset> 
+				</form>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-actions">
+							<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+							<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
 						</div>
-					</div>	
-					<div class="form-group">
-						<label class="col-sm-2 control-label" ></label>
-						<div class="col-sm-9">
-							<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-							<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+					</fieldset>
+				</form>
+			</div>
+			<div class="col-sm-6">
+				<table id="table_Calibration" class="table table-bordered table-condensed tablesorter"></table>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-actions">
+							<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+							<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-2 control-label">{{Catégorie}}</label>
-						<div class="col-md-8">
-							<?php
-								foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-									echo '<label class="checkbox-inline">';
-									echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-									echo '</label>';
-								}
-							?>
+					</fieldset>
+				</form>
+			</div>
+		</div>		
+		<div class="right">
+			<div class="col-sm-6">
+				<legend>{{Commande d'entrée du neurone}}</legend>
+				<table id="table_cmd_Entree" class="table table-bordered table-condensed">
+					<thead>
+				                <tr>
+				                    	<th>{{Commande}}</th>
+							<!--th>{{Tolérance}}</th-->
+							<th>{{Action}}</th>
+				                </tr>
+				        </thead>
+				        <tbody></tbody>
+				</table>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-actions">
+							<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+							<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label" >{{Calibration}}</label>
-						<div class="col-sm-9">
-							<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Calibration}}" data-l1key="configuration" data-l2key="calibration" />
+					</fieldset>
+				</form>
+			</div>
+			<div class="col-sm-6">
+				<legend>{{Commande de sortie du neurone}}</legend>
+				<table id="table_cmd_Sortie" class="table table-bordered table-condensed">
+					<thead>
+				                <tr>
+				                    	<th>{{Commande}}</th>
+							<!--th>{{Tolérance}}</th-->
+							<th>{{Action}}</th>
+				                </tr>
+				        </thead>
+				        <tbody></tbody>
+				</table>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-actions">
+							<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+							<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
 						</div>
-					</div>
-				</div>
-			</fieldset> 
-		</form>
-		<form class="form-horizontal">
-			<fieldset>
-				<div class="form-actions">
-					<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-					<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-				</div>
-			</fieldset>
-		</form>
-		<div class="col-sm-6">
-			<legend>{{Commande d'entrée du neurone}}</legend>
-			<table id="table_cmd_Entree" class="table table-bordered table-condensed">
-				<thead>
-			                <tr>
-			                    	<th>{{Commande}}</th>
-						<!--th>{{Tolérance}}</th-->
-						<th>{{Action}}</th>
-			                </tr>
-			        </thead>
-			        <tbody></tbody>
-			</table>
-		</div>
-		<div class="col-sm-6">
-			<legend>{{Commande de sortie du neurone}}</legend>
-			<table id="table_cmd_Sortie" class="table table-bordered table-condensed">
-				<thead>
-			                <tr>
-			                    	<th>{{Commande}}</th>
-						<!--th>{{Tolérance}}</th-->
-						<th>{{Action}}</th>
-			                </tr>
-			        </thead>
-			        <tbody></tbody>
-			</table>
+					</fieldset>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
