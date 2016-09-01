ba6281c5d310fd3130b72906605983adfdb410c0
mika-nt28 <michaelhalbout@gmail.com>
2016-09-01 13:26:24 +0200

    Update install.sh

--- ressources/install.sh
+++ ressources/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo 1 > /tmp/compilation_Neuronal_in_progress
-sudo apt-get install -y --force-yes libfann-dev 
-echo 45 > /tmp/compilation_Neuronal_in_progress
-sudo pecl install g++
-echo 70 > /tmp/compilation_Neuronal_in_progress
-sudo pecl install fann
+sudo apt-get install -y --force-yes libfann-dev cmake
+echo 10 > /tmp/compilation_Neuronal_in_progress
+git clone https://github.com/libfann/fann.git
+cd ./fann
+cmake
+sudo make install
 echo 90 > /tmp/compilation_Neuronal_in_progress
 if grep -q "extension=fann.so" /etc/php5/apache2/php.ini  then
     echo "L'extention fann.so est présent";
