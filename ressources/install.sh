b12c5c34132541d8ecb0374d5bdcddbb3828d734
mika-nt28 <michaelhalbout@gmail.com>
2016-09-01 13:19:52 +0200

    Update install.sh

--- ressources/install.sh
+++ ressources/install.sh
@@ -6,6 +6,10 @@ sudo pecl install g++
 echo 70 > /tmp/compilation_Neuronal_in_progress
 sudo pecl install fann
 echo 90 > /tmp/compilation_Neuronal_in_progress
-/etc/init.d/nginx restart
+if grep -q "extension=fann.so" /etc/php5/apache2/php.ini  then
+    echo "L'extention fann.so est présent";
+else
+  sudo echo "extension=fann.so" >> /etc/php5/apache2/php.ini;
+fi
 echo 100 > /tmp/compilation_Neuronal_in_progress
 rm /tmp/compilation_Neuronal_in_progress
