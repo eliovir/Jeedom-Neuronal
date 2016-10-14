429a74fd17a929364805ee624a6f662be8643fef
Halbout Michael <michaelhalbout@gmail.com>
2016-10-14 09:17:35 +0200

    Update Neuronal.php

--- desktop/php/Neuronal.php
+++ desktop/php/Neuronal.php
@@ -78,8 +78,10 @@ $eqLogics = eqLogic::byType('Neuronal');
 							<div class="form-group">
 								<label class="col-sm-2 control-label" ></label>
 								<div class="col-sm-9">
-									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+									<label>{{Activer}}</label>
+									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+									<label>{{Visible}}</label>
+									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
 								</div>
 							</div>
 							<div class="form-group">
@@ -97,7 +99,7 @@ $eqLogics = eqLogic::byType('Neuronal');
 							<div class="form-group">
 								<label class="col-sm-2 control-label" >{{Calibration}}</label>
 								<div class="col-sm-9">
-									<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Calibration}}" data-l1key="configuration" data-l2key="calibration" />
+									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Calibration}}" data-l1key="configuration" data-l2key="calibration" />
 								</div>
 							</div>
 						</div>
