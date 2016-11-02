fd7e96eb9ce64dcd3cb8f50621d6ab30d08650c7
Halbout Michael <michaelhalbout@gmail.com>
2016-11-02 13:22:06 +0100

    Update Neuronal.php

--- desktop/php/Neuronal.php
+++ desktop/php/Neuronal.php
@@ -164,14 +164,6 @@ $eqLogics = eqLogic::byType('Neuronal');
 					</div>	
 				</div>
 			</div>
-			<form class="form-horizontal">
-				<fieldset>
-					<div class="form-actions">
-						<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-						<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-					</div>
-				</fieldset>
-			</form>
 		</div>	
 	</div>
 </div>
