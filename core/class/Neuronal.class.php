2ec5f145e61051dd732312548203d48162dba7bc
Halbout Michael <michaelhalbout@gmail.com>
2016-11-04 10:38:20 +0100

    Update Neuronal.class.php

--- core/class/Neuronal.class.php
+++ core/class/Neuronal.class.php
@@ -72,6 +72,7 @@ class Neuronal extends eqLogic {
 			$Commande->setName($Name);
 			$Commande->setLogicalId($_logicalId);
 			$Commande->setEqLogic_id($eqLogic->getId());
+			$Commande->setConfiguraiton('Commandes','');
 			$Commande->setType('info');
 			$Commande->setSubType('other');
 		}
