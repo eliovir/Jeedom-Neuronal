7c1788e4a1e20e23834930a0d19071ec3d446237
Halbout Michael <michaelhalbout@gmail.com>
2016-11-04 11:02:23 +0100

    Update Neuronal.js

--- desktop/js/Neuronal.js
+++ desktop/js/Neuronal.js
@@ -24,12 +24,10 @@ function saveEqLogic(_eqLogic) {
 	}	
 	if (typeof( _eqLogic.cmd) !== 'undefined') {
 		for(var index in  _eqLogic.cmd) { 
-			if (typeof( _eqLogic.cmd[index].configuration) !== 'undefined') 
-				_eqLogic.cmd[index].configuration=new Object();
 			if (typeof( _eqLogic.cmd[index].configuration.ListeCommandes) !== 'undefined') 
 				_eqLogic.cmd[index].configuration.ListeCommandes=new Object();
 			var Commandes= new Array();
-			$('.cmd[data-cmd_id=' + init(_eqLogic.cmd[index].id)+ '] .ConditionGroup').each(function( index ) {
+			$('.cmd[data-cmd_id=' + init(_eqLogic.cmd[index].id)+ '] .CommandeGroup').each(function( index ) {
 				Commandes.push($(this).getValues('.expressionAttr')[0])
 			});
 			_eqLogic.cmd[index].configuration.ListeCommandes=Commandes;
