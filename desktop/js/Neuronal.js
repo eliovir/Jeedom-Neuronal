288332e9daae2044206e8ed315f9d23a3a09fcdf
Halbout Michael <michaelhalbout@gmail.com>
2017-02-07 16:02:56 +0100

    Update Neuronal.js

--- desktop/js/Neuronal.js
+++ desktop/js/Neuronal.js
@@ -91,7 +91,7 @@ function addElement(_Commande, _el) {
         _el.find('tr:last').setValues(_Commande, '.expressionAttr');
 }
 function addCalibration(_Table, _el){
-	var tr=_el.find('thead th').clone().insertAfter("tbody");
+	var tr=_el.find('thead th').clone();
 	tr.find('th').each(function(index){
 		 $(this).replaceWith($('<td>').append($('<input class="CalibraionAttr" data-l1key="'+$(this).attr('data-param')+'">')));
 	});
